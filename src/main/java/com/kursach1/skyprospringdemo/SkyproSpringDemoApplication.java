aca3dde47cce5d03b59ddb908a36891edbffc55a
vmorozov893 <vmorozov893@gmail.com>
2024-10-15 21:29:24 +0300

    начало

---
+++ src/main/java/com/kursach1/skyprospringdemo/SkyproSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package com.kursach1.skyprospringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringDemoApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/kursach1/skyprospringdemo/SkyproSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursach1.skyprospringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
